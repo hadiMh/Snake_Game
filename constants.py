689ff8485498d2cef189f2f26edd2744554efbf0
MohammadHadi Hajihosseini <m.hadi.hajihosseini@gmail.com>
2021-05-29 03:37:49 +0430

    add constants.py file to save constant values.

---
+++ constants.py
@@ -0,0 +1,13 @@
+class Colors:
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    BLUE = (0, 0, 255)
+
+    # Material Colors
+    M_BLUE_LIGHT = (178, 235, 242)
+    M_BLUE = (41, 182, 246)
+    M_PINK = (216, 27, 96)
+
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
